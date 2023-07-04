befacc677c33712d25630b0f52b33b7596ea5779
mbron770 <mbronfin.tsd@gmail.com>
2023-07-04 17:49:24 -0400

    complete deliverables

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,26 @@
 # your code goes here!
+from time import sleep
+
+
+def countdown(x):
+    # while x > 0:
+    #     print(f'{x} SECOND(S)!')
+    #     x-=1
+    # print('HAPPY NEW YEAR!')
+
+    for x in range(x, 0, -1):
+        print(f'{x} SECOND(S)!')
+    print('HAPPY NEW YEAR!')
+
+
+def countdown_with_sleep(x):
+    # while x > 0:
+    #     print(f'{x} SECOND(S)!')
+    #     sleep(1)
+    #     x-=1
+    # print('HAPPY NEW YEAR!')
+
+    for x in range(x, 0, -1):
+        print(f'{x} SECOND(S)!')
+        sleep(1)
+    print('HAPPY NEW YEAR!')
